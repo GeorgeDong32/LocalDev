66c07a79689bb20af4dd0000390b489662575950
GeorgeDong32 <georgedong32@foxmail.com>
2022-09-29 15:23:06 +0800

    Test

--- ClionTest/Test Py/Test1.py
+++ ClionTest/Test Py/Test1.py
@@ -1,1 +1,9 @@
 # Copyright (c)  GeorgeDong32. All Rights Reserved.
+import matplotlib.pyplot as plt
+import numpy as np
+
+print("Hello,world!")
+print("Test end!")
+x = [0, 5]
+plt.plot(x, np.sin(x))
+plt.show()
