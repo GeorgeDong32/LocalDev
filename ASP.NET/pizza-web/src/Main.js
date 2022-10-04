002280cdde37341b5927a4751e75cc0bf01671e3
GeorgeDong32 <georgedong32@foxmail.com>
2022-10-04 22:54:45 +0800

    Update

--- ASP.NET/pizza-web/src/Main.js
+++ ASP.NET/pizza-web/src/Main.js
@@ -1,4 +1,34 @@
 import React, { useState } from "react";
+import styled from "styled-components";
+
+const PizzaFrame = styled.div`
+    border: solid 1px gray;
+    padding: 10px;
+    margin: 15px 10px;
+    border-radius: 5px;
+    box-shadow: 0 0 5px grey;
+    font-family: Arial;
+`;
+
+const Input = styled.input`
+    border: solid 1px black;
+    padding: 5px;
+    border-radius: 3px;
+`;
+
+const Title = styled(Input)`
+    text-transform: uppercase;
+`;
+
+const Save = styled.button`
+   width: 100px;
+   margin: 10px;
+   background: green;
+   color: white;
+   font-size: 16px;
+   padding: 10px;
+   border-radius: 5px;
+`;
 
 let pizzas = [{
     id: 1, name: 'Cheese pizza', description: 'very cheesy'
@@ -18,22 +48,22 @@ const Pizza = ({ pizza }) => {
 
     function onSave() {
         setDirty(false);
-        // make rest call
+        // make REST call
     }
 
     return (<React.Fragment>
-        <div>
+        <PizzaFrame>
             <h3>
-                <input onChange={(evt) => update(evt.target.value, 'name', data)} value={data.name} />
+                <Title onChange={(evt) => update(evt.target.value, 'name', data)} value={data.name} />
             </h3>
             <div>
-                <input onChange={(evt) => update(evt.target.value, 'description', data)} value={data.description} />
+                <Input onChange={(evt) => update(evt.target.value, 'description', data)} value={data.description} />
             </div>
             {dirty ?
-                <div><button onClick={onSave}>Save</button></div> : null
+                <div><Save onClick={onSave}>Save</Save></div> : null
             }
-        </div>
-    </React.Fragment>);
+        </PizzaFrame>
+    </React.Fragment>)
 }
 
 const Main = () => {
