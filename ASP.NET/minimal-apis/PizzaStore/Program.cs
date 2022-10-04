345ffbd21f5c316663ffa380d03661ef39a9ecb9
GeorgeDong32 <georgedong32@foxmail.com>
2022-10-04 20:06:37 +0800

    add put

--- ASP.NET/minimal-apis/PizzaStore/Program.cs
+++ ASP.NET/minimal-apis/PizzaStore/Program.cs
@@ -24,12 +24,23 @@ app.UseSwaggerUI(c =>
 });
 
 app.MapGet("/pizzas", async (PizzaDb db) => await db.Pizzas.ToListAsync());
+app.MapGet("/pizza/{id}", async (PizzaDb db, int id) => await db.Pizzas.FindAsync(id));
 
 app.MapPost("/pizza", async (PizzaDb db, Pizza pizza) =>
 {
     await db.Pizzas.AddAsync(pizza);
     await db.SaveChangesAsync();
     return Results.Created($"/pizza/{pizza.Id}", pizza);
+});
+
+app.MapPut("/pizza/{id}", async (PizzaDb db, Pizza updatepizza, int id) =>
+{
+    var pizza = await db.Pizzas.FindAsync(id);
+    if (pizza is null) return Results.NotFound();
+    pizza.Name = updatepizza.Name;
+    pizza.Description = updatepizza.Description;
+    await db.SaveChangesAsync();
+    return Results.NoContent();
 });
 
 app.Run();
