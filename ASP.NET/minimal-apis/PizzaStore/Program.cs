1fe8384ac03eaaa603e2d6b04606f97db2ce2ac4
GeorgeDong32 <georgedong32@foxmail.com>
2022-10-04 20:01:53 +0800

    Update

--- ASP.NET/minimal-apis/PizzaStore/Program.cs
+++ ASP.NET/minimal-apis/PizzaStore/Program.cs
@@ -1,8 +1,11 @@
 using Microsoft.OpenApi.Models;
+using Microsoft.EntityFrameworkCore;
+using PizzaStore.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddDbContext<PizzaDb>(options => options.UseInMemoryDatabase("items"));
 builder.Services.AddSwaggerGen(c =>
 {
     c.SwaggerDoc("v1", new OpenApiInfo
@@ -20,6 +23,6 @@ app.UseSwaggerUI(c =>
     c.SwaggerEndpoint("/swagger/v1/swagger.json", "PizzaStore API V1");
 });
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/pizzas", async (PizzaDb db) => await db.Pizzas.ToListAsync());
 
 app.Run();
