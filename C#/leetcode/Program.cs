44869cd65b51f2a3f7d70b96756426955bf86dba
GeorgeDong32 <georgedong32@foxmail.com>
2022-10-05 23:19:18 +0800

    Leetcode 1

--- C#/leetcode/Program.cs
+++ C#/leetcode/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*A+B*/
+public class Solution
+{
+    public int[] TwoSum(int[] nums, int target)
+    {
+        int[] re = new int[2]; int start = 0;
+        re[0] = 0; re[1] = 0;
+        foreach (int num in nums)
+        {
+            for (int i = start + 1; i < nums.Length; i++)
+            {
+                if (num + nums[i] == target)
+                {
+                    re[0] = start;
+                    re[1] = i;
+                    return re;
+                }
+            }
+            start++;
+        }
+        return re;
+    }
+}
