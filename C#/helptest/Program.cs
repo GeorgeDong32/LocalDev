379abaa6ade6c90c889c0fabcd5b16f7c6033a13
GeorgeDong32 <georgedong32@foxmail.com>
2022-10-07 10:24:41 +0800

    Init

---
+++ C#/helptest/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Test
+{
+    class test
+    {
+        public void swap(ref int x, ref int y)
+        {
+            int temp;
+            temp = x;
+            x = y;
+            y = temp;
+        }
+
+        static void Main(string[] args)
+        {
+            int num = 0;
+            num = Convert.ToInt32(Console.ReadLine());
+            System.Console.WriteLine("Hello World!", num);
+            int age = 19;
+            string name = "张三丰";
+            Console.WriteLine("你好，我叫" + name + "我今年" + age + "岁了");
+
+        }
+    }
+}
