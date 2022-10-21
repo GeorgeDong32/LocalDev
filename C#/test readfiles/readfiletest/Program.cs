6baee6e708f2f7757ce2c08add05e4e5dafe2c65
GeorgeDong32 <georgedong32@foxmail.com>
2022-10-21 22:46:12 +0800

    Test read file success

--- C#/test readfiles/readfiletest/Program.cs
+++ C#/test readfiles/readfiletest/Program.cs
@@ -1,1 +1,11 @@
-﻿
+﻿string userName = Environment.UserName;
+
+string _appdataPath = $"C:\\Users\\{userName}\\AppData\\Local";
+
+Console.WriteLine(_appdataPath);
+
+string LocalMKPath = Path.Combine(_appdataPath, "GoodPass", "MKconfig.txt");
+
+var LocalMKHash = File.ReadAllText(LocalMKPath);
+
+Console.WriteLine(LocalMKHash);
