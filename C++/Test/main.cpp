acfd336510c8c57457c8f101a6102d9ae8fbdd86
GeorgeDong32 <georgedong32@foxmail.com>
2022-10-29 17:30:26 +0800

    Update main.cpp

--- C++/Test/main.cpp
+++ C++/Test/main.cpp
@@ -6,23 +6,20 @@ int main()
     int n = 0;
     cin >> n;
     int* num = new int[n];
-    int** maxAdress = &num;
+    int* maxAdress = &num[0];
     cout << "num adress: " << num << endl;
     for(int i = 0; i < n; i++)
         cin >> num[i];
     for(int i = 0; i < n; i++)
     {
-        cout << "before: num[" << i << "] = " << num[i] << "\t&num[" << i << "] = " << &num[i];
-        cout << "\t**A = " << **maxAdress << "\t*A = " << *maxAdress << endl;
-        if(**maxAdress < num[i])
+        cout << "before: num[" << i << "] = " << num[i] << "\t&num[" << i << "] = " << &num[i] << endl;
+        if(*maxAdress < num[i])
         {
-            *maxAdress = &num[i];
+            maxAdress = &num[i];
             cout << num << endl;
         }
-        cout << "After: num[" << i << "] = " << num[i] << "\t&num[" << i << "] = " << &num[i];
-        cout << "\t**A = " << **maxAdress << "\t*A = " << *maxAdress << endl;
     }
-    cout << "the max num is " << **maxAdress << "and its adress is " << *maxAdress << endl;
+    cout << "the max num is " << *maxAdress << "and its adress is " << maxAdress << endl;
     delete[] num;
     return 0;
 }
