0e3b5590f54b2d8fd13e560bbc349577808d79b8
George·Dong <georgedong32@foxmail.com>
2022-12-06 11:08:48 +0800

    Update

--- C++/Test/main.cpp
+++ C++/Test/main.cpp
@@ -5,18 +5,6 @@ using namespace std;
 
 int main()
 {
-    Solution s = Solution();
-    vector<vector<char>> test(6,vector<char>(6));
-    char temp = 0;
-    for(int i = 0; i < 6; i++)
-    {
-        for(int j = 0; j < 6; j++)
-        {
-            cin >> temp;
-            test[i].push_back(temp);
-        }
-    }
-    int re = s.maximalSquare(test);
-    cout << "Result: " << re << endl;
+    cout << "Hello World!" << endl;
     return 0;
 }
