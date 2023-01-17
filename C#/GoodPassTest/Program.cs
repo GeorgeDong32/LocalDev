f782e33001f27f708811040564bb30c034fdb2e9
George·Dong <georgedong32@foxmail.com>
2023-01-17 23:07:01 +0800

    Test

--- C#/GoodPassTest/Program.cs
+++ C#/GoodPassTest/Program.cs
@@ -1,3 +1,4 @@
-﻿using System;
-var urlString = "http://test";
-var url = new Uri(urlString);
+﻿var test1 = "280000000000000000";
+var test2 = "999";
+var tr1 = Convert.ToInt32(test1);
+var tr2 = Convert.ToInt32(test2);
