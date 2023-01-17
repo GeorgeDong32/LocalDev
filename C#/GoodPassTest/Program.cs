da866160121cfce147ffced74b308c1ec5b74da1
George·Dong <georgedong32@foxmail.com>
2023-01-17 15:00:49 +0800

    Test uri

--- C#/GoodPassTest/Program.cs
+++ C#/GoodPassTest/Program.cs
@@ -1,10 +1,3 @@
-﻿using GoodPass.Services;
-
-var crypt = new GoodPassCryptographicServices();
-var testStr = "TestGoodPassSES";
-Console.WriteLine($"Test string: {testStr}");
-var encStr = crypt.EncryptStr(testStr);
-Console.WriteLine($"Encrypted string: {encStr}");
-var decStr = crypt.DecryptStr(encStr);
-Console.WriteLine($"Decrypted string: {decStr}");
-Console.WriteLine($"Test string == Decrypted string: {testStr == decStr}");
+﻿using System;
+var urlString = "http://test";
+var url = new Uri(urlString);
