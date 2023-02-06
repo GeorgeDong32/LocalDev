438bb867f78a14a529f818f5b5cbb4f338626da8
George·Dong <georgedong32@foxmail.com>
2023-02-06 18:10:57 +0800

    Update Program.cs

--- C#/GoodPassTest/Program.cs
+++ C#/GoodPassTest/Program.cs
@@ -1,4 +1,8 @@
-﻿var test1 = "280000000000000000";
-var test2 = "999";
-var tr1 = Convert.ToInt32(test1);
-var tr2 = Convert.ToInt32(test2);
+﻿var time = new DateTime(2022, 12, 2, 23, 32, 59);
+var timePatch1 = (char)(48 + time.Month + time.Minute);
+var timePatch2 = (char)(48 + time.Hour + time.Day);
+var timePatch = timePatch1.ToString() + timePatch2.ToString();
+Console.WriteLine(timePatch);
+Console.WriteLine((int)timePatch1);
+Console.WriteLine((int)timePatch2);
+Console.WriteLine((char)126);
