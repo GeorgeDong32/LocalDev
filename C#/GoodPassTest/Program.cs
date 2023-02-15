98307b89c50be6e2900ab96584ce43e03e179000
George·Dong <georgedong32@foxmail.com>
2023-02-15 20:43:25 +0800

    Update Program.cs

--- C#/GoodPassTest/Program.cs
+++ C#/GoodPassTest/Program.cs
@@ -1,1 +1,16 @@
-﻿
+﻿var teststr = "Test";
+var teststr2 = "Test2";
+var teststr3 = "Test3";
+var tarstr = "test";
+if (teststr.ToLower().Contains(tarstr.ToLower()))
+{
+    Console.WriteLine("Teststr contains tarstr");
+}
+if (teststr2.Contains(tarstr))
+{
+    Console.WriteLine("Teststr2 contains tarstr");
+}
+if (teststr3.Contains(tarstr))
+{
+    Console.WriteLine("Teststr3 contains tarstr");
+}
