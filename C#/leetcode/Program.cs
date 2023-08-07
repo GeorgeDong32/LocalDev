0ed27ddee71270d3ae5d0e6a89292a2645f01885
George·Dong <georgedong32@foxmail.com>
2023-08-07 21:15:37 +0800

    Q20

--- C#/leetcode/Program.cs
+++ C#/leetcode/Program.cs
@@ -1,55 +1,18 @@
-﻿public class Solution
-{
-    public int RomanToInt(string s)
-    {
-        var result = 0;
-        //预处理
-        s = s.Replace("IV", "a");
-        s = s.Replace("IX", "b");
-        s = s.Replace("XL", "c");
-        s = s.Replace("XC", "d");
-        s = s.Replace("CD", "e");
-        s = s.Replace("CM", "f");
-        //处理
-        for (int i = 0; i < s.Length; i++)
-        {
-            result += GetValue(s[i]);
-        }
-        return result;
-    }
+﻿var solution = new Solution();
 
-    public int GetValue(char c)
+Console.WriteLine(solution.IsValid("((((()))))"));
+Console.WriteLine(solution.IsValid("{[(())]}"));
+
+public class Solution
+{
+    public bool IsValid(string s)
     {
-        switch (c)
+        while (s.Contains("()") || s.Contains("[]") || s.Contains("{}"))
         {
-            case 'a':
-                return 4;
-            case 'b':
-                return 9;
-            case 'c':
-                return 40;
-            case 'd':
-                return 90;
-            case 'e':
-                return 400;
-            case 'f':
-                return 900;
-            case 'I':
-                return 1;
-            case 'V':
-                return 5;
-            case 'X':
-                return 10;
-            case 'L':
-                return 50;
-            case 'C':
-                return 100;
-            case 'D':
-                return 500;
-            case 'M':
-                return 1000;
-            default:
-                return 0;
+            s = s.Replace("()", "");
+            s = s.Replace("[]", "");
+            s = s.Replace("{}", "");
         }
+        return s == "";
     }
 }
