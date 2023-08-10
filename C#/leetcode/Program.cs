feefcf922ffd6f5524d380f747aa5c11820be8dd
George·Dong <georgedong32@foxmail.com>
2023-08-10 22:26:14 +0800

    Q69

--- C#/leetcode/Program.cs
+++ C#/leetcode/Program.cs
@@ -1,49 +1,11 @@
-﻿/*Q67*/
+﻿/*Q69*/
 var solution = new Solution();
-Console.WriteLine(solution.AddBinary("111", "10"));
 
+/*API战神*/
 public class Solution
 {
-    public string AddBinary(string a, string b)
+    public int MySqrt(int x)
     {
-        var ml = a.Length > b.Length ? a.Length : b.Length;
-        var result = ""; var carry = 0;
-        if (a.Length != ml)
-        {
-            for (var i = a.Length; i < ml; i++)
-            {
-                a = "0" + a;
-            }
-        }
-        else if (b.Length != ml)
-        {
-            for (var i = b.Length; i < ml; i++)
-            {
-                b = "0" + b;
-            }
-        }
-        for (var i = ml - 1; i >= 0; i--)
-        {
-            var mid = 0;
-            if (a[i] == '1' && b[i] == '1')
-            {
-                mid = 0 + carry;
-                carry = 1;
-            }
-            else if ((a[i] == '1' && b[i] == '0') || (b[i] == '1' && a[i] == '0'))
-            {
-                mid = (1 + carry) % 2;
-                carry = (1 + carry) / 2;
-            }
-            else
-            {
-                mid = carry;
-                carry = 0;
-            }
-            result = ((mid == 1) ? "1" : "0") + result;
-        }
-        if (carry == 1)
-            result = "1" + result;
-        return result;
+        return (int)Math.Sqrt(x);
     }
 }
