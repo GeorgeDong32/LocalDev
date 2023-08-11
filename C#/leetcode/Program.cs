c92c1a7b7bc81206fd094576dbdd8cac6f940c3a
George·Dong <georgedong32@foxmail.com>
2023-08-11 08:42:28 +0800

    Q83

--- C#/leetcode/Program.cs
+++ C#/leetcode/Program.cs
@@ -1,11 +1,39 @@
-﻿/*Q69*/
+﻿/*Q83*/
 var solution = new Solution();
+Console.WriteLine(solution.DeleteDuplicates(new ListNode(1, new ListNode(1, new ListNode(2)))));
 
-/*API战神*/
+public class ListNode
+{
+    public int val;
+    public ListNode next;
+    public ListNode(int val = 0, ListNode next = null)
+    {
+        this.val = val;
+        this.next = next;
+    }
+}
+
 public class Solution
 {
-    public int MySqrt(int x)
+    public ListNode DeleteDuplicates(ListNode head)
     {
-        return (int)Math.Sqrt(x);
+        if (head is null)
+            return null;
+        var current = head;
+        var next = current.next;
+        while (next != null)
+        {
+            if (current.val != next.val)
+            {
+                current = next;
+                next = next.next;
+            }
+            else
+            {
+                current.next = next.next;
+                next = current.next;
+            }
+        }
+        return head;
     }
 }
